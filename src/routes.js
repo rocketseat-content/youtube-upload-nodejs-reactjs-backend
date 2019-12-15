6b16f8e61be131830a32aad5d09036e916c1b6ee
Felipe Barros <felipetech@tuta.io>
2019-12-15 19:37:45 -0300

    Adicionar o controller de uploads

--- src/routes.js
+++ src/routes.js
@@ -2,33 +2,12 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
 
-const Post = require("./models/Post");
+const uploadController = require('./controllers/uploadController');
 
-routes.get("/posts", async (req, res) => {
-  const posts = await Post.find();
+routes.get("/posts", uploadController.list);
 
-  return res.json(posts);
-});
+routes.post("/posts", multer(multerConfig).single("file"), uploadController.create);
 
-routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
-  const { originalname: name, size, key, location: url = "" } = req.file;
-
-  const post = await Post.create({
-    name,
-    size,
-    key,
-    url
-  });
-
-  return res.json(post);
-});
-
-routes.delete("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-
-  await post.remove();
-
-  return res.send();
-});
+routes.delete("/posts/:id", uploadController.delete);
 
 module.exports = routes;
